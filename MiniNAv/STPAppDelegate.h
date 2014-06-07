491c6c64092557ef58663e97ca050cf9dcd1d8e3
SaveThePlan <realnanook@gmail.com>
2014-06-07 14:21:52 +0200

    Project Creation

---
+++ MiniNAv/STPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STPAppDelegate.h
+//  MiniNAv
+//
+//  Created by Nanook on 07/06/2014.
+//  Copyright (c) 2014 SaveThePlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiniNAv/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MiniNAv
+//
+//  Created by Nanook on 07/06/2014.
+//  Copyright (c) 2014 SaveThePlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STPAppDelegate class]));
+    }
+}
